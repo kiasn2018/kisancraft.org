c684e095fac230ab1caa191dcfebce29dc742df7
kiasn2018 <38281209+kiasn2018@users.noreply.github.com>
2018-04-11 16:31:47 +0530

    intial commit
    pushing all files

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+// date base connection
+$servername = "localhost";
+$username = "kisan";
+$password = "yes";
+$dbname = "branch_mst";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
