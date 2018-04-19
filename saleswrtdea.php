5c960f3b79365a5a934058def997213c3bbee1d3
kiasn2018 <38281209+kiasn2018@users.noreply.github.com>
2018-04-19 10:09:56 +0530

    Export big data into excel

--- saleswrtdea.php
+++ saleswrtdea.php
@@ -1,6 +1,9 @@
 <?php ini_set('max_execution_time', 0);?>
 <html>
 <head>
+
+		
+	
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	<script src="/kisankraft.org/src/js/sorttable.js"></script>
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
@@ -107,6 +110,7 @@ function getdealer(val) {
 	});
 	}
 </script>
+
 <div style="margin-left:22%; width:75%;">
 <?php 
 $results_per_page = 100;
@@ -173,7 +177,52 @@ if(!isset($_POST["go"]) ){
    ?>
    <tbody>
   </table>
-  <?php
+<button onclick="exportTableToCSV('members.csv')">Export HTML Table To CSV File</button>
+ <script>
+  function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+  </script>
+   <?php
+  
 $sql = "SELECT COUNT(ID) AS total FROM Salesmst";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
@@ -301,5 +350,50 @@ if($_POST["go"]=="Submit"){
                 <?php }?> 
 				</table>
                 </div>
+				<script>
+  function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+  </script>
+				<button onclick="exportTableToCSV('members.csv')">Export HTML Table To CSV File</button>
+
                 <?php 
    }}
